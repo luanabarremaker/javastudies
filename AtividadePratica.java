d60e053a91a17bc73ce0e65e03e9fd88d1a0cae0
Luana Barremaker Ruschel <123482142+luanabarremaker@users.noreply.github.com>
2024-06-04 19:44:45 -0300

    Add files via upload
    error: failed to push some refs to 'https://github.com/luanabarremaker/javastudies.git'

---
+++ AtividadePratica.java
@@ -0,0 +1,22 @@
+//Atividade Pratica 13 
+
+class AtividadePratica
+{
+   public static void main (String entrada[])
+   {
+	String nome = "Luana Barremaker";
+	String curso = "Engenharia de Software";
+	int idade = 19;
+	char sexo = 'F';
+	double peso = 55.4;
+	boolean VF = true;
+
+     if (VF == true)
+     {
+	System.out.println("Eu sou a " + nome + " do genero " + sexo + ". Curso " + curso + ", tenho " + idade + " anos e peso " + peso + "kg.");
+     }
+
+    System.exit(0);
+    
+   }
+}
